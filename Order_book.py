895ed776cc4f0d5d56788366c2d46ca002ebb65f
raul11217732 <98422580+raul11217732@users.noreply.github.com>
2022-12-06 22:11:26 +0530

    Update Order_book.py

--- Order_book.py
+++ Order_book.py
@@ -1,7 +1,13 @@
-tree = ET.parse(filepath)
+tree = ET.parse('path')
 
 root = tree.getroot()
 
+i = 0
 for elem in root:
+    if i>10:
+        break
     for subelem in elem:
+        if i>10:
+            break
+        i+=1
         print(subelem.txt)
